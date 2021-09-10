7d431e732f15eb53464ad94a393fd2a3dd525f58
Tuong Duy <63220379+tuongduy10@users.noreply.github.com>
2021-09-10 22:58:42 +0700

    new change

--- main.js
+++ main.js
@@ -4,35 +4,86 @@ const apiUrl = 'https://owsnews.herokuapp.com/covid?fbclid=IwAR2NUuX2nrVl87DEyGU
 function fetchData(){
     fetch(apiUrl)
         .then(function (response){
+            // Check response after fetching url
+            if(!response.ok){
+                document.getElementById('item-source').innerHTML = "Lỗi khi tải dữ liệu vui lòng thử lại!"
+            }
             return response.json()
         })
         .then(function (data){
-            const dulieu = data['data']
-            const nguon = data['source_covid']
 
-            const htmls = dulieu.map(item =>{
+            // Nguồn: Bộ y tế.....
+            const nguon = data['source_covid']
+            document.getElementById('item-source').innerHTML = nguon
+
+
+            // Data: tinh, ca nhiem, ca tu vong, ...
+            const dulieu = data['data']
+            dulieu.sort((i,j) => {
+                var striTong = i.tong_nhiem
+                var strjTong = j.tong_nhiem
+
+                // replace . in string and parse into integer
+                // ex: "14.576" => "14576" => 14576
+                var iTong = parseInt(striTong.replace('.',''))
+                var jTong = parseInt(strjTong.replace('.',''))
+
+                return jTong - iTong
+            })
+            const html = dulieu.map(item =>{
                 var index = dulieu.findIndex(i => i.tinh == item.tinh)
                 index++
+
                 return `<ul id="data-list"> <h3> ${index}. ${item.tinh}</h3> 
                             <li>Ca nhiễm trong ngày: ${item.nhiem}</li>
                             <li>Ca tử vong trong ngày: ${item.tuvong}</li>
                             <li>Tổng ca nhiễm: ${item.tong_nhiem}</li>
                             <li>Tổng ca tử vong: ${item.tong_tuvong}</li>
                         </ul>`
-            })
+            }).join('')
+            document.getElementById('item-data').innerHTML = html
 
-            const htmlData = htmls.join('')
 
-            document.getElementById('item-data').innerHTML = htmlData
-            document.getElementById('item-source').innerHTML = nguon
+            // total death
+            var totalDeath = 0
+            for(var item of dulieu){
+                var str = item.tong_tuvong
+                var val = parseInt(str.replace('.',''))
+                totalDeath += val
+            }
+            document.getElementById('total-death').innerHTML = `<h3>Tổng ca tử vong toàn quốc: </h3> ${totalDeath}`
+
+            // total death in day
+            var totalDeathInDay = 0
+            for(var item of dulieu){
+                var str = item.tuvong
+                var val = parseInt(str.replace('.',''))
+                totalDeathInDay += val
+            }
+            document.getElementById('total-death-inday').innerHTML = `<h3>Tổng ca tử vong toàn quốc trong ngày: </h3>${totalDeathInDay}`
+
+            // total infected
+            var totalInfected = 0
+            for(var item of dulieu){
+                var str = item.tong_nhiem
+                var val = parseInt(str.replace('.',''))
+                totalInfected += val
+            }
+            document.getElementById('total-infected').innerHTML = `<h3>Tổng ca nhiễm toàn quốc: </h3>${totalInfected}`
+            
+            // total infected in day
+            var totalInfectedInDay = 0
+            for(var item of dulieu){
+                var str = item.nhiem
+                var val = parseInt(str.replace('.',''))
+                totalInfectedInDay += val
+            }
+            document.getElementById('total-infected-inday').innerHTML = `<h3>Tổng ca nhiễm trong ngày toàn quốc: </h3>${totalInfectedInDay}`
             
         })
-
- 
 }
+fetchData()
 
 function test(){
-    alert('hello')
+    alert('test')
 }
-
-fetchData()
